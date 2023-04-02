84eb7cc6b991b037da087309c157155d0ab70087
kunpeng <1968538974@qq.com>
2023-04-02 23:51:54 +0800

    添加工具依赖包

--- owen-web/src/main/java/com/zust/owen/OwenApplication.java
+++ owen-web/src/main/java/com/zust/owen/OwenApplication.java
@@ -1,6 +1,8 @@
 package com.zust.owen;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OwenApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(OwenApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(OwenApplication.class, args);
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        logger.error("error logback for austin");
+        logger.info("info logback for austin");
         return String.format("Hello %s!", name);
     }
 }
